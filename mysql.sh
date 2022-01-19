2b0612d3d1437ab2b6c11e3dd910527c20021aa7
myounis789 <mohammedyounis789@gmail.com>
2022-01-19 22:18:13 +0000

    Updated images in script with newly built images

--- mysql.sh
+++ mysql.sh
@@ -1,8 +1,8 @@
 docker system prune -a
-docker pull myounis789/mysqltest:latest
-docker pull myounis789/nginxtest:latest
-docker pull myounis789/frontendtest:latest
-docker pull myounis789/backendtest:latest
+docker pull myounis789/angular:latest
+docker pull myounis789/mysql:latest
+docker pull myounis789/nginxpetclinic:latest
+docker pull myounis789/rest2:latest
 docker-compose up -d
 docker exec -it MySQL bash
 
