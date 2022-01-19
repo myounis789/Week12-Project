50b07a641a6b8056d76052e244a782b0d86b5e4d
myounis789 <mohammedyounis789@gmail.com>
2022-01-19 19:15:53 +0000

    Updated deploy script with further steps

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,8 @@ curl https://get.docker.com | sudo bash
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-node -version
+sudo chmod 666 /var/run/docker.sock
+node --version
 npm -v
-docker -version
+docker --version
 docker-compose -v
